70d27ea5cb41930f034ea4e8987fc5d57646cbe4
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2024-02-14 03:11:58 +0530

    fix:parser id y

--- utils/generateLocationId.js
+++ utils/generateLocationId.js
@@ -5,7 +5,7 @@ const generateLocationId = async (homeName) => {
   let locationId = "";
   while (!unique) {
     const randomNumber = Math.floor(1000 + Math.random() * 9000); // Generates a 4-digit random number
-    locationId = `${homeName}${randomNumber}@ylocateme`; // Concatenate the home name and the random number
+    locationId = `${homeName}${randomNumber}@locateme`; // Concatenate the home name and the random number
     const existingUser = await User.findOne({ locationId });
     if (!existingUser) {
       unique = true;
