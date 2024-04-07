26733ddf8124d65362c4ca452eb7ed0f01dc0559
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2024-04-07 17:57:09 +0530

    fix:genrate id

--- utils/generateLocationId.js
+++ utils/generateLocationId.js
@@ -5,7 +5,8 @@ const generateLocationId = async (homeName) => {
   let locationId = "";
   while (!unique) {
     const randomNumber = Math.floor(1000 + Math.random() * 9000); // Generates a 4-digit random number
-    locationId = `${homeName}${randomNumber}@locateme`; // Concatenate the home name and the random number
+    const sanitizedHomeName = homeName.replace(/\s+/g, ""); // Removes spaces from homeName
+    locationId = `${sanitizedHomeName}${randomNumber}@locateme`; // Concatenate the sanitized home name and the random number
     const existingUser = await User.findOne({ locationId });
     if (!existingUser) {
       unique = true;
