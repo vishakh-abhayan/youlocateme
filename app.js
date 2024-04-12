a7e0b9ce4befbc4c4d1cd6db469fabf6fbdc92d6
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2024-04-13 00:23:48 +0530

    fix:cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const apiRoutes = require("./routes/api");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
